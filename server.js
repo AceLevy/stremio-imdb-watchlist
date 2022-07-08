e4d833efa49942ca8427175f404f570af89a9ea7
AceLevy <57027645+AceLevy@users.noreply.github.com>
2022-07-08 13:39:39 +0800

    use PORT variable, if available

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const app = require('./index.js')
 
 // create local server
-app.listen(7505, function () {
-    console.log('Addon active on port 7505.');
-    console.log('http://127.0.0.1:7505/[imdb-user-id]/manifest.json');
+serverPort = (process.env.PORT || 7505)
+
+app.listen(serverPort, function () {
+    console.log('Addon active on port ' + serverPort + '.');
+    console.log('http://127.0.0.1:'+serverPort+'/[imdb-user-id]/manifest.json');
 });
